3de2315a5e59fbd9c4a981c16e73558356600119
solominh <hoangminhftu2@gmail.com>
2017-01-09 03:53:15 +0700

    Clean up

--- send_keys_from_image.py
+++ send_keys_from_image.py
@@ -1,32 +1,36 @@
+
 import keyboard
 import pyautogui
 import cv2
 from get_image_region import draw_region
 from subprocess import call
 
-# Take screenshot
-screenshot_path = './screenshot.png'
-pyautogui.screenshot(screenshot_path)
 
-# Draw image region
-image = cv2.imread(screenshot_path)
-ref_points = draw_region(image)
-print(ref_points)
+def main():
+    # Take screenshot
+    screenshot_path = './screenshot.png'
+    pyautogui.screenshot(screenshot_path)
 
-# Sanity check
-if not ref_points:
-    print('Region not selected')
-    exit(0)
+    # Draw image region
+    image = cv2.imread(screenshot_path)
+    ref_points = draw_region(image)
+    print(ref_points)
 
-# Save cropped image
-cropped_image = image[ref_points['topleft'][1]:ref_points['bottomright'][1],
-                      ref_points['topleft'][0]:ref_points['bottomright'][0]]
-cv2.imwrite(screenshot_path, cropped_image)
+    # Sanity check
+    if not ref_points:
+        print('Region not selected')
+        return
 
-# Convert image to text
-text_ouput_path = './text_from_image'
-call(["tesseract", screenshot_path, text_ouput_path])
+    # Save cropped image
+    cropped_image = image[ref_points['topleft'][1]:ref_points['bottomright'][1],
+                          ref_points['topleft'][0]:ref_points['bottomright'][0]]
+    cv2.imwrite(screenshot_path, cropped_image)
 
-with open(text_ouput_path + '.txt') as f:
-    print(f)
-    # keyboard.write(data.strip())
+    # Convert image to text
+    text_ouput_path = './text_from_image'
+    call(["tesseract", screenshot_path, text_ouput_path])
+
+    # Open result file
+    with open(text_ouput_path + '.txt') as f:
+        print(f)
+        # keyboard.write(data.strip())
