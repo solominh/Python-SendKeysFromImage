4758e37d33e7017ea1a10ebfde15a3ee47c0f64a
solominh <hoangminhftu2@gmail.com>
2017-01-09 03:24:14 +0700

    App working now

--- send_keys_from_image.py
+++ send_keys_from_image.py
@@ -1,24 +1,32 @@
-# import pyautogui
 import keyboard
 import pyautogui
 import cv2
 from get_image_region import draw_region
+from subprocess import call
 
+# Take screenshot
 screenshot_path = './screenshot.png'
 pyautogui.screenshot(screenshot_path)
 
+# Draw image region
 image = cv2.imread(screenshot_path)
 ref_points = draw_region(image)
 print(ref_points)
-if ref_points:
-   pass
 
+# Sanity check
+if not ref_points:
+    print('Region not selected')
+    exit(0)
 
-def send_keys(data):
-    keyboard.write(data.strip())
-    # pyautogui.typewrite(data.strip())
+# Save cropped image
+cropped_image = image[ref_points['topleft'][1]:ref_points['bottomright'][1],
+                      ref_points['topleft'][0]:ref_points['bottomright'][0]]
+cv2.imwrite(screenshot_path, cropped_image)
 
+# Convert image to text
+text_ouput_path = './text_from_image'
+call(["tesseract", screenshot_path, text_ouput_path])
 
-def main():
-    send_keys(data)
-    print('Send keys successfully!')
+with open(text_ouput_path + '.txt') as f:
+    print(f)
+    # keyboard.write(data.strip())
